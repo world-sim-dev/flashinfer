330c0aaefca8514a9e88006dc89b4ba8a34d23a4
HongyuJia <jiahongyu0825@gmail.com>
2025-02-14 21:14:57 +0800

    Jit compile

--- flashinfer/customfn.py
+++ flashinfer/customfn.py
@@ -1,41 +1,73 @@
 from typing import Optional
 
 import torch
-from .utils import get_cuda_stream
 
-# mypy: disable-error-code="attr-defined"
-try:
-    from . import _kernels
-except ImportError as e:
-    import logging
-    import os
+from .jit import FLASHINFER_CSRC_DIR, has_prebuilt_ops, load_cuda_ops
+from .utils import get_cuda_stream, register_custom_op, register_fake_op
 
-    if os.environ.get("BUILD_DOC", "0") == "1":
-        _kernels = None
-        logging.warning("Kernels are not loaded in documentation build mode.")
-    else:
-        raise e
+_custom_fn_module = None
 
 
-def _check_shape_and_dtype(input: torch.Tensor, output: torch.Tensor, output_dtype: torch.dtype):
+def get_custom_fn_module():
+    global _custom_fn_module
+    if _custom_fn_module is None:
+        if has_prebuilt_ops:
+            from . import _kernels
+
+            _custom_fn_module = _kernels
+        else:
+            _custom_fn_module = load_cuda_ops(
+                "customfn",
+                [
+                    FLASHINFER_CSRC_DIR / "flashinfer_custom_ops.cu",
+                    FLASHINFER_CSRC_DIR / "customfn.cu",
+                ],
+            )
+    return _custom_fn_module
+
+
+@register_custom_op("flashinfer::div_clamp_to", mutates_args=("out",))
+def _div_clamp_to(out: torch.Tensor, input: torch.Tensor, scale: torch.Tensor) -> None:
+    with input.device as device:  # device guard
+        get_custom_fn_module().div_clamp_to(out, input, scale, get_cuda_stream(device))
+
+
+@register_fake_op("flashinfer::div_clamp_to")
+def _div_clamp_to_fake(
+    out: torch.Tensor, input: torch.Tensor, scale: torch.Tensor
+) -> None:
+    pass
+
+
+def _check_shape_and_dtype(
+    input: torch.Tensor, output: torch.Tensor, output_dtype: torch.dtype
+):
     assert input.ndim == output.ndim, f"{input.ndim} != {output.ndim}"
     assert input.shape == output.shape, f"{input.shape} != {output.shape}"
-    assert input.dtype in {torch.bfloat16, torch.float16}, \
-        f"{input.dtype} != {torch.bfloat16} or {torch.float16}"
+    assert input.dtype in {
+        torch.bfloat16,
+        torch.float16,
+    }, f"{input.dtype} != {torch.bfloat16} or {torch.float16}"
     assert output.dtype == output_dtype, f"{output.dtype} != {output_dtype}"
 
-def div_clamp_to(input: torch.Tensor, scale: torch.Tensor, output_dtype: torch.dtype, out: torch.Tensor = None) -> torch.Tensor:
+
+def div_clamp_to(
+    input: torch.Tensor,
+    scale: torch.Tensor,
+    output_dtype: torch.dtype,
+    out: torch.Tensor = None,
+) -> torch.Tensor:
     r"""Fused div, clamp and to operation.
     Parameters
     ----------
     input: torch.Tensor
         Input tensor, shape: (..., hidden_size), dtype: torch.bfloat16 or torch.float16
-    
+
     scale: torch.Tensor
         Scale tensor, shape: (hidden_size), dtype: torch.float32
     output_dytpe: torch.dtype
         Output dtype: torch.float8_e4m3fn or torch.float8_e5m2.
-    
+
     out: Optional[torch.Tensor]
         The the output tensor, if specified, the kernel will update this tensor inplace.
 
@@ -51,7 +83,9 @@ def div_clamp_to(input: torch.Tensor, scale: torch.Tensor, output_dtype: torch.d
         _check_shape_and_dtype(input, out, output_dtype)
     else:
         out = torch.empty(
-            input.shape, device=input.device, dtype=output_dtype,
+            input.shape,
+            device=input.device,
+            dtype=output_dtype,
         )
-    _kernels.div_clamp_to(out, input, scale.flatten(), get_cuda_stream(input.device))
+    _div_clamp_to(out, input, scale.flatten())
     return out
